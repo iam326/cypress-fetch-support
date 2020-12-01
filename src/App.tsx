339f61326b0d7ef22b97f14b1900c0f4eef75361
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-12-02 01:06:40 +0900

    add employee list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import './App.css';
 
+type Employee = {
+  id: number;
+  name: string;
+  age: number;
+};
+
 const App: FC = () => {
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const listStyle = {
+    marginBottom: '4px',
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:3001/employees')
+      .then((response) => response.json())
+      .then((data) => setEmployees(data));
+  }, []);
+
   return (
     <div className="App">
+      <h3>Employees</h3>
       <ul>
-        <li>hoge</li>
-        <li>fuga</li>
-        <li>piyo</li>
+        {employees.map((employee) => (
+          <li key={employee.id} style={listStyle}>
+            {employee.name} ({employee.age})
+          </li>
+        ))}
       </ul>
     </div>
   );
